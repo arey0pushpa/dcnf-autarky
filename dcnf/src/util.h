05557cc70803d006fd957d9d620037473d2a800e
ankit <ankitk@ncbs.res.in>
2018-06-28 12:05:59 +0200

    indentation fix util.cpp

--- dcnf/src/util.h
+++ dcnf/src/util.h
@@ -131,23 +131,20 @@ inline int find_scd_index(Vec1Dpair& vec, int elem) {
 
 /** Trim Operations */
 // trim from left
-inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
-{
-    s.erase(0, s.find_first_not_of(t));
-    return s;
+inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v") {
+  s.erase(0, s.find_first_not_of(t));
+  return s;
 }
 
 // trim from right
-inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
-{
-    s.erase(s.find_last_not_of(t) + 1);
-    return s;
+inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v") {
+  s.erase(s.find_last_not_of(t) + 1);
+  return s;
 }
 
 // trim from left & right
-inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
-{
-    return ltrim(rtrim(s, t), t);
+inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v") {
+  return ltrim(rtrim(s, t), t);
 }
 /** Print n-dimentional vector **/
 inline void print_1d_vector(Vec1D& vec) {
