3aa9bd1c30bc5080927da5a88de1d59922872141
ankitk <ankitk@ncbs.res.in>
2019-04-24 08:33:22 +0200

    added unsat example

--- dcnf/src/main.cpp
+++ dcnf/src/main.cpp
@@ -344,7 +344,7 @@ int main(int argc, char *argv[]) {
     iter_active_evars.clear();
 
 		if (d->active_evars.size() == 0) {
-			std::cout << "All univ variable case." << '\n';
+			std::cout << "All univ variable case. Fml SAT." << '\n';
 			// Send it to a SAT solver?
 			exit(0);
 		}
