b1b779241e44b90134469fcb55bad281e9eb721b
ankitk <ankitk@ncbs.res.in>
2019-05-15 22:24:37 +0200

    renaming the file

--- dcnf/cmd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "filename pn pc sha1 autarky assmt result time" > "file.out" 
-for file in ./examples/dqbf18/*
-do
-	echo "$file" >> "file.out"
-  ./dcnf_autarky -i "$file" -r 3 >> "file.out"
-	echo "" >> "file.out"
-done
