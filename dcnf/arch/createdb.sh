0eeb67273a11df25522746684bb21f6200a3729f
ankit <ankitk@ncbs.res.in>
2019-09-04 03:24:30 +0200

    Fixed next line overflow

--- dcnf/arch/createdb.sh
+++ dcnf/arch/createdb.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Download the files instances locally
+# settings / change this to your config
 #git clone https://qbflib.sagelab.it/qbflib/qdimacs
-git clone https://qbflib.sagelab.it/qbflib/dqbf
+#git clone https://qbflib.sagelab.it/qbflib/dqbf
 
 mkdir -p Autarkies Database Experiments
 
 # Create a database: direcetory structure similar to tht of the instances
+# TODO: Handle Path stuff and make each of them variable
 cd qdimacs
 find . -type d -o -type f -exec bash -c '
   for path; do mkdir -p "../Database/${path/file/folder}"; done
@@ -25,7 +27,10 @@ do
       while IFS= read -r line;do
         if [[ $line =~ ^[[:space:]]*p.* ]]; then
           arr=($line)
-          echo -n "${arr[2]} " > ../Database${i#.}/${i##*/} 
+          echo -n "${i##*/} " > ../Database${i#.}/${i##*/}
+          echo -n ${i##*/} | sha1sum | awk '{printf $1}' >> ../Database${i#.}/${i##*/}
+          echo -n " " >> ../Database${i#.}/${i##*/} 
+          echo -n "${arr[2]} " >> ../Database${i#.}/${i##*/} 
           echo -n ${arr[3]} >> ../Database${i#.}/${i##*/}
           break
         fi
